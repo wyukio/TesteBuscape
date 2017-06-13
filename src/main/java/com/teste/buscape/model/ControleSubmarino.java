be5aa57f9c7035878f6b7e0c10e732f88e34d30b
Fernando Watanabe <fernandoyukiowatanabe@hotmail.com>
2017-06-12 21:38:01 -0300

    Criando classe que controla o submarino

---
+++ src/main/java/com/teste/buscape/model/ControleSubmarino.java
@@ -0,0 +1,32 @@
+package com.teste.buscape.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.teste.buscape.exceptions.ComandoInvalidoException;
+import com.teste.buscape.exceptions.SubmarinoAcimaDoNivelDoMarException;
+
+public class ControleSubmarino {
+	private static final Map<String,Acao> ACOES;
+	
+	static{
+		ACOES = new HashMap<String, Acao>();
+		ACOES.put("L", new VirarParaEsquerda());
+		ACOES.put("R", new VirarParaDireita());
+		ACOES.put("U", new Subir());
+		ACOES.put("D", new Descer());
+		ACOES.put("M", new Mover());
+	}
+	
+	public static void controlarSubmarino(String comando, Submarino submarino) throws SubmarinoAcimaDoNivelDoMarException, ComandoInvalidoException{
+		Acao acao = ACOES.get(comando);
+		
+		if(acao == null)
+			throw new ComandoInvalidoException();
+		
+		acao.realizarMovimento(submarino);
+		
+		if(submarino.posicao.z > 0)
+			throw new SubmarinoAcimaDoNivelDoMarException();
+	}
+}
