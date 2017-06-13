f0c5d2713406c2c9d76855124d63eb96bc7b79f0
Fernando Watanabe <fernandoyukiowatanabe@hotmail.com>
2017-06-12 21:32:52 -0300

    Implementando troca de direcao e movimento do submarino

--- src/main/java/com/teste/buscape/model/Direcao.java
+++ src/main/java/com/teste/buscape/model/Direcao.java
@@ -1,8 +1,72 @@
 package com.teste.buscape.model;
 
 public enum Direcao {
-	NORTE,
-	SUL,
-	LESTE,
-	OESTE
+	NORTE{
+		@Override
+		public Direcao virarParaEsquerda() {
+			return Direcao.OESTE;
+		}
+
+		@Override
+		public Direcao virarParaDireita() {
+			return Direcao.LESTE;
+		}
+
+		@Override
+		public void mover(Submarino submarino) {
+			submarino.posicao.y++;
+		}
+	},
+ 	SUL{
+		@Override
+		public Direcao virarParaEsquerda() {
+			return Direcao.LESTE;
+		}
+
+		@Override
+		public Direcao virarParaDireita() {
+			return Direcao.OESTE;
+		}
+
+		@Override
+		public void mover(Submarino submarino) {
+			submarino.posicao.y--;
+		}
+	},
+ 	LESTE {
+		@Override
+		public Direcao virarParaEsquerda() {
+			return Direcao.NORTE;
+		}
+
+		@Override
+		public Direcao virarParaDireita() {
+			return Direcao.SUL;
+		}
+
+		@Override
+		public void mover(Submarino submarino) {
+			submarino.posicao.x++;
+		}
+	},
+	OESTE{
+		@Override
+		public Direcao virarParaEsquerda() {
+			return Direcao.SUL;
+		}
+
+		@Override
+		public Direcao virarParaDireita() {
+			return Direcao.NORTE;
+		}
+
+		@Override
+		public void mover(Submarino submarino) {
+			submarino.posicao.x--;
+		}
+	};
+	
+	public abstract Direcao virarParaEsquerda();
+	public abstract Direcao virarParaDireita();
+	public abstract void mover(Submarino submarino);
 }
